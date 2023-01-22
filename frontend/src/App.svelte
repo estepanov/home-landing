974b879cff357aa98a4f994f20953fe505c21da6
Evans A. Stepanov <evans@stepanov.email>
2023-01-22 03:00:01 -0500

    cleanup page style add dark mode support

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,60 +1,62 @@
 <script module="ts">
-   import { onMount } from 'svelte';
- 
- let date = new Date();
- const MONTH = {
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: "May",
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-  12: "December"
- }
- const DAYS = {
-  0: "Sunday",
-  1: "Monday",
-  2: "Tuesday",
-  3: "Wednesday",
-  4: "Thursday",
-  5: "Friday",
-  6: "Saturday"
- }
- $: dayOfWeek = `${DAYS[date.getDay()]}`;
- $: dateString = `${MONTH[date.getMonth() + 1]} ${date.getDate()}, ${date.getFullYear()}`;
-//  $: hour = date.getHours();
-//  $: min = date.getMinutes();
-//  $: sec = date.getSeconds();
+  import { LightSwitch } from "@skeletonlabs/skeleton"
+  import { onMount } from "svelte"
 
- $: time = date.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })
+  let date = new Date()
+  const MONTH = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  }
+  const DAYS = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  }
+  $: dayOfWeek = `${DAYS[date.getDay()]}`
+  $: dateString = `${MONTH[date.getMonth() + 1]} ${date.getDate()}, ${date.getFullYear()}`
 
- 
- onMount( () => {
-   const interval = setInterval(() => {
-     date = new Date();
-   }, 1000);
-   return () => clearInterval(interval);
- });
+  $: time = date.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  })
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      date = new Date()
+    }, 1000)
+    return () => clearInterval(interval)
+  })
 </script>
 
-<main>
-  <div class="absolute top-5 right-5 flex flex-col justify-center items-end">
-    <!-- <div class="text-5xl font-bold">
-      {hour}:{min < 10 ? '0' :''}{min}<span class="text-surface-300 font-mono text-lg">:{sec < 10 ? '0' :''}{sec}</span>
-    </div> -->
-    <div class="text-4xl font-bold">
+<main class="bg-gradient-to-bl from-primary-500 to-transparent h-full w-full">
+  <div class="absolute top-5 right-5 flex flex-col justify-center items-end space-y-1">
+    <div class="text-4xl font-bold text-white">
       {time}
     </div>
-    <div class="text-surface-400">
+    <div class="text-token text-md">
       {dateString}
     </div>
-    <div class="text-surface-400">
+    <div class="text-token text-sm">
       {dayOfWeek}
     </div>
+  </div>
+
+  <div class="absolute bottom-5 right-5 flex flex-col justify-center items-end">
+    <LightSwitch />
   </div>
 </main>
